a3964db2ad2adfdf7fec860f848e2c2648d207b5
ryan kurte <ryan@kurte.nz>
2019-10-30 09:09:33 +1300

    initial commit, untested, good luck

---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+
+use std::io::{Write, BufRead, BufReader, Error as IoError};
+use std::collections::HashMap;
+
+#[macro_use]
+extern crate log;
+
+#[macro_use]
+extern crate serde;
+extern crate serde_json;
+
+extern crate serial;
+use serial::prelude::*;
+use serial::SystemPort;
+
+pub struct Connector {
+    port: BufReader<SystemPort>,
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum Message {
+    #[serde(rename = "LOC")]
+    Location(Location),
+
+    #[serde(rename = "RAW")]
+    Raw(Raw),
+
+    #[serde(rename = "MSG")]
+    Message(DebugMessage),
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Location {
+    pub x: f32,
+    pub y: f32,
+    pub h: Option<f32>,
+    pub mode: String,
+    pub update_rate: usize,
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Raw {
+    pub accel_x: f32,
+    pub accel_y: f32,
+    pub accel_z: f32,
+
+    pub gyro_x: f32,
+    pub gyro_y: f32,
+    pub gyro_z: f32,
+
+    pub mag_x: f32,
+    pub mag_y: f32,
+    pub mag_z: f32,
+
+    pub sampling_rate: usize,
+}
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct DebugMessage {
+    pub body: String,
+    pub data: HashMap<String, String>,
+}
+
+impl Connector {
+    pub fn new(port: &str, baud: usize) -> Result<Self, IoError> {
+        let p = std::path::Path::new(port);
+
+        let mut port = serial::open(p)?;
+
+        port.reconfigure(&|settings| {
+            settings.set_baud_rate(serial::BaudRate::from_speed(baud))?;
+            Ok(())
+        })?;
+
+        Ok(Self{port: BufReader::new(port)})
+    }
+ 
+    pub fn poll(&mut self) -> Result<Option<Message>, IoError> {
+
+        // Read line from serial port
+        let mut s = String::default();
+        let _n = self.port.read_line(&mut s)?;
+
+        debug!("read line: {}", s);
+
+        // Attempt to decode
+        match serde_json::from_str(&s) {
+            Ok(v) => return Ok(Some(v)),
+            Err(e) => {
+                warn!("Error decoding line: {:?}", e);
+                warn!("{}", s);
+                return Ok(None)
+            }
+        }
+    }
+
+    pub fn write(&mut self, s: &str) -> Result<(), IoError> {
+        let p = self.port.get_mut();
+
+        p.write(s.as_bytes())?;
+
+        Ok(())
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_decode_location() {
+        let s = r#"{ "type": "LOC", "x": 11.22, "y": 33.44, "h":1.57, "mode": "IMU", "update_rate": 100 }"#;
+        
+        let m: Message = serde_json::from_str(s).unwrap();
+
+        if let Message::Location(loc) = &m {
+
+        } else {
+            assert_eq!(1, 2);
+        }
+    }
+
+    #[test]
+    fn test_decode_debug_message() {
+        let s = r#"{ "type": "RAW","accel_x": 11.22, "accel_y": 33.44, "accel_z": 55.66, "gyro_r": 11.22, "gyro_y": 33.44, "gyro_z": 55.66, "mag_x": 11.22, "mag_y": 33.44, "mag": 55.66,"sampling_rate": 100 }"#;
+        
+        let m: Message = serde_json::from_str(s).unwrap();
+
+        if let Message::Raw(raw) = &m {
+
+        } else {
+            assert_eq!(1, 2);
+        }
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+
+#[macro_use] extern crate log;
+extern crate simplelog;
+use simplelog::{SimpleLogger, LevelFilter};
+
+extern crate structopt;
+use structopt::StructOpt;
+
+extern crate cs704_ui;
+use cs704_ui::*;
+
+#[derive(Clone, Debug, PartialEq, StructOpt)]
+pub struct Options {
+    #[structopt(short = "p", long = "serial.port", default_value = "/dev/serial0", env = "GPS_PORT")]
+    /// Serial port for receiving NMEA data
+    pub serial_port: String,
+
+    #[structopt(short = "b", long = "serial.baud", default_value = "115200", env = "GPS_BAUD")]
+    /// Client ID for MQTT connection
+    pub baud: usize,
+
+    #[structopt(short = "z", long = "rezero")]
+    /// Zero location based on first location packet
+    pub rezero: bool,
+
+    #[structopt(short = "i", long = "init-command")]
+    /// Init command to be sent to the device
+    pub init_command: Option<String>,
+
+    #[structopt(short = "m", long = "mode-filter")]
+    /// Location mode type to filter on
+    pub mode_filter: Option<String>,
+
+    #[structopt(long = "log-level", default_value = "info")]
+    /// Enable verbose logging
+    pub level: LevelFilter,
+}
+
+fn main() {
+    // Load options
+    let opts = Options::from_args();
+
+    // Setup logging
+    SimpleLogger::init(opts.level, simplelog::Config::default()).unwrap();
+
+    // Connect to serial port
+    debug!("Opening serial port ({}, {} baud)", opts.serial_port, opts.baud);
+    let mut c = Connector::new(&opts.serial_port, opts.baud).unwrap();
+
+    // Send init command if set
+    if let Some(command) = &opts.init_command {
+        c.write(command).unwrap();
+    }
+
+    let mut zero_x = 0.0;
+    let mut zero_y = 0.0;
+    let mut zeroed = false;
+
+    loop {
+        // Poll for new messages
+        let m = match c.poll().unwrap() {
+            Some(m) => m,
+            None => continue,
+        };
+
+        // Handle incoming messages
+        match &m {
+            Message::Message(DebugMessage{body, data}) => {
+                info!("Debug: {}, ({:?})", body, data);
+            },
+            Message::Raw(raw) => {
+                info!("Raw: {:?}", raw);
+            },
+            Message::Location(loc) => {
+
+                // Filter by location mode if specified
+                if let Some(filter) = &opts.mode_filter {
+                    if filter != &loc.mode {
+                        continue;
+                    }
+                }
+
+                // Apply zeroing if requested
+                if opts.rezero && !zeroed {
+                    info!("Applying new zero position X: {:.2}, Y: {:.2}", loc.x, loc.y);
+                    zero_x = loc.x;
+                    zero_y = loc.y;
+                    zeroed = true;
+                }
+
+                // Extract and display X, Y, Mode
+                let (x, y, mode) = (loc.x - zero_x, loc.y - zero_y, &loc.mode);
+                print!("\r X: {:.2} Y: {:.2} Mode: {}", x, y, mode);
+            },
+        }
+    }
+
+}
